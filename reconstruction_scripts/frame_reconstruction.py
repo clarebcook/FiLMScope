e8641fcfce9b8001f804f24f1bd177a8921601f4
Clare Cook <cbc55@duke.edu>
2024-10-31 16:01:42 -0400

    incorporate neptune logging

--- reconstruction_scripts/frame_reconstruction.py
+++ reconstruction_scripts/frame_reconstruction.py
@@ -4,10 +4,17 @@ from tqdm import tqdm
 sample_name = "skull_with_tool"
 gpu_number = "0"
 
-config_dict = generate_config_dict(sample_name=sample_name, gpu_number=gpu_number, downsample=3,
-                                   camera_set="all", run_args={"iters": 150})
+config_dict = generate_config_dict(sample_name=sample_name, gpu_number=gpu_number, downsample=4,
+                                   camera_set="all", use_neptune=True,
+                                   run_args={"iters": 10, "batch_size": 12, "num_depths": 64,
+                                             "display_freq": 5},)
 run_manager = RunManager(config_dict)
 
 iters = config_dict["run_args"]["iters"]
+losses = []
 for i in tqdm(range(iters)):
-    res = run_manager.run_epoch(i)
+    log = (i % config_dict["run_args"]["display_freq"] == 0) or (i == iters - 1)
+    mask_images, warp_images, numbers, outputs, loss_values = run_manager.run_epoch(i, log=log)
+    losses.append(float(loss_values["total"]))
+
+run_manager.end()
