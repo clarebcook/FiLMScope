0bfd98c03d706cf84d6b4b0303d37ab3817e63cb
Clare Cook <cbc55@duke.edu>
2024-12-27 16:28:50 -0500

    simplify for datasets not using all images

--- filmscope/util/display_functions.py
+++ filmscope/util/display_functions.py
@@ -98,14 +98,15 @@ def play_video(frames_array, fps=30):
 
 # TODO: right now this is hardcoded for our expected 6x8 layout
 def get_preview_image(image_filename, blank_filename=None, downsample=5, border_size=10,
-                      *args, **kwargs):
-    image_numbers = np.arange(48) 
+                      image_numbers=None, *args, **kwargs):
+    if image_numbers is None:
+        image_numbers = np.arange(48) 
     image_layout = (6, 8) 
     images = load_image_set(image_filename, image_numbers=image_numbers,
                             blank_filename=blank_filename, downsample=downsample, *args, **kwargs)
     
         # for now assume all images are the same shape
-    image_shape = images[0].shape
+    image_shape = images[image_numbers[0]].shape
     if len(image_shape) == 3:
         full_image_shape = (
             image_layout[0] * (image_shape[0] + border_size) + border_size,
